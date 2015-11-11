7490311c6de5b75216930c84930e5a05e1296aab
Rahil <rahil.dedhia@students.olin.edu>
2015-11-10 20:26:56 -0500

    adding initial control script

---
+++ initial_control/initial_control.ino
@@ -0,0 +1,74 @@
+#include "DualMC33926MotorShield.h"
+
+#define pinA 2
+#define pinB 5
+#define numTicks 1216
+
+// Create the motor shield object with the default I2C address
+DualMC33926MotorShield md;
+
+// Encoder stuff (counters and ticks)
+volatile int mCounter;
+
+// For converting from encoder ticks to angles
+const float mDivider = numTicks / 360.;
+
+// Angle measures
+float mAngle;
+
+// PySerial stuff
+String incoming = "";
+String panString = "";
+String tiltString = "";
+
+// Motor control
+int maxVelocity = 250;
+
+// Motor velocities
+double mVelocity;
+
+// Timing
+int time;
+int lastTime;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(pinA, INPUT);
+  pinMode(pinB, INPUT);
+  // enable internal pullup resistors
+  digitalWrite(pinA, 1);
+  digitalWrite(pinB, 1);
+  // using encoders with interrupts
+  attachInterrupt(0, ISR_motor, CHANGE);
+  // Start motor shield
+  md.init();
+  //tell PySerial you're ready to go
+  Serial.print("Initialized!");
+}
+
+void loop() {
+  // calculating angle from counters by scaling it
+  mAngle = (mCounter % numTicks) / mDivider;
+  
+  // hard code speed at 100
+  md.setM2Speed(100);
+
+  time = millis();
+  // Print stuff every half second
+  if (time-lastTime > 500) {
+    Serial.print(mAngle);
+    Serial.print(mCounter);
+    Serial.print('\n');
+    lastTime = time;
+  }
+}
+
+// Interrupt sequences to increment counters on CHANGE of encoder pins
+void ISR_motor()
+{
+  if (digitalRead(pinA) == digitalRead(pinB)) {
+    mCounter++;
+  } else {
+    mCounter--;
+  }
+}
